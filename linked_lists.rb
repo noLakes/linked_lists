0bea195adbc6bf59ce18213f74a040ac95dff4c6
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-05-29 23:43:40 -0400

    initial commit, built node and list classes, as well as init methods and append

---
+++ linked_lists.rb
@@ -0,0 +1,39 @@
+
+class Node
+  attr_accessor :val, :next_node
+
+  def initialize(val=nil, next_node=nil)
+    @val = val
+    @next_node = next_node
+  end
+
+  def tail?
+    self.next_node == nil
+  end
+end
+
+class LinkedList
+  attr_accessor :head
+  def initialize(val=1)
+    @head = Node.new(val, nil)
+  end
+
+  def append(val)
+    if head.tail?
+      head.next_node = Node.new(val)
+    else
+      head.next_node.append(val)
+    end
+  end
+end
+
+x = LinkedList.new('greg')
+
+p x
+
+x.append('jeff')
+
+p x
+
+
+
