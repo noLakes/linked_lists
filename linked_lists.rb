e4ee9da8faf00d320fc765739a0b4b8320ec2ffd
“noLakes” <51928001+noLakes@users.noreply.github.com>
2020-05-30 15:02:21 -0400

    added @tmp variable to store last read node, wrote at(idx) method

--- linked_lists.rb
+++ linked_lists.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 class Node
   attr_accessor :val, :next_node
+  attr_reader :pos
 
   def initialize(val=nil, next_node=nil)
     @val = val
@@ -14,19 +15,35 @@ class Node
 end
 
 class LinkedList
-  attr_accessor :list, :read
+  attr_accessor :list, :read, :tmp
   def initialize(val=nil)
     @list = Node.new(val, nil)
     @read = @list
-    @count = 0
+    @tmp = @read
+    @pos = 0
   end
 
   def read_next
     @read = @read.next_node
+    @tmp = @read
+    @pos += 1
   end
 
   def reset_read
     @read = @list
+    @pos = 0
+  end
+
+  def head 
+    "{ #{list.val} }"
+  end
+
+  def tail
+    until read.tail? do
+      read_next
+    end
+    reset_read
+    "{ #{tmp.val} }"
   end
 
   def append(val)
@@ -34,7 +51,7 @@ class LinkedList
       read.next_node = Node.new(val)
       reset_read
     else
-      @read = @read.next_node
+      read_next
       append(val)
     end
   end
@@ -46,7 +63,13 @@ class LinkedList
   end
 
   def size
-    
+    count = 1
+    until read.tail? do
+      count += 1
+      read_next
+    end
+    reset_read
+    count
   end
 
   def to_s
@@ -54,11 +77,24 @@ class LinkedList
     loop do
       string << "{ #{read.val} }  =>"
       break if read.tail?
-      @read = read.next_node
+      read_next
     end
     reset_read
     string << 'nil'
     string.join(' ')
+  end
+
+  def at(idx)
+    return "Error: Index[#{idx}] equals or exceeds list length." if idx >= self.size
+    until @pos == idx do
+      read_next
+    end
+    reset_read
+    "{ #{tmp.val} }"
+  end
+
+  def pop
+    
   end
 
 end
@@ -72,6 +108,4 @@ x.prepend('bob')
 
 
 puts x.to_s
-
-
-
+puts x.size
